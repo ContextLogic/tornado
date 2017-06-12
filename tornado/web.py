69b4524a9792fba3f2817ee9303b9b6db67cfaee
Nidheesh Puthalath <nidheesh@contextlogic.com>
2017-06-12 14:29:40 -0700

    Modified the way tornado authenticator decorator is i implemented so that this can be customized.
    Summary:
    Modified the way tornado authenticator decorator is i implemented so that this can be customized
    
    Blame:
    
    Test Plan: Check in sweeper by overriding this method and makesure if this working. test in default call by calling super class implementation to makesure there is no change in the authenticator behavior
    
    Reviewers: jim, tarek
    
    Reviewed By: tarek
    
    Differential Revision: https://phab.wish.com/D46693

--- tornado/web.py
+++ tornado/web.py
@@ -1012,6 +1012,22 @@ class RequestHandler(object):
     def _ui_method(self, method):
         return lambda *args, **kwargs: method(self, *args, **kwargs)
 
+    def handle_authenticated(self, method, *args, **kwargs):
+        if not self.current_user:
+            if self.request.method in ("GET", "HEAD"):
+                url = self.get_login_url()
+                if "?" not in url:
+                    if urlparse.urlsplit(url).scheme:
+                        # if login url is absolute, make next absolute too
+                        next_url = self.request.full_url()
+                    else:
+                        next_url = self.request.uri
+                    url += "?" + urllib.urlencode(dict(next=next_url))
+                self.redirect(url)
+                return
+            raise HTTPError(403)
+        return method(self, *args, **kwargs)
+
 
 def asynchronous(method):
     """Wrap request handler methods with this if they are asynchronous.
@@ -1661,20 +1677,7 @@ def authenticated(method):
     """Decorate methods with this to require that the user be logged in."""
     @functools.wraps(method)
     def wrapper(self, *args, **kwargs):
-        if not self.current_user:
-            if self.request.method in ("GET", "HEAD"):
-                url = self.get_login_url()
-                if "?" not in url:
-                    if urlparse.urlsplit(url).scheme:
-                        # if login url is absolute, make next absolute too
-                        next_url = self.request.full_url()
-                    else:
-                        next_url = self.request.uri
-                    url += "?" + urllib.urlencode(dict(next=next_url))
-                self.redirect(url)
-                return
-            raise HTTPError(403)
-        return method(self, *args, **kwargs)
+        return self.handle_authenticated(method, *args, **kwargs)
     return wrapper
 
 
